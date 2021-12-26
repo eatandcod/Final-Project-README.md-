1e5441cdd212673772a8daa1047e80d239bdf6f9
eatandcod <95302587+eatandcod@users.noreply.github.com>
2021-12-26 13:26:29 +0600

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("books.fxml"));
+        Scene scene = new Scene(myLoader.load());
+        stage.setTitle("Mualim!");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
